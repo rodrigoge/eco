7b2f6ba5da26e94f9a829a23ed1260f3be672c23
Rodrigo Gouveia <rgestevao@gmail.com>
2023-03-26 10:39:19 -0300

    feat: starting projects in application

---
+++ api/pointservice/src/main/java/br/com/eco/pointservice/PointserviceApplication.java
@@ -0,0 +1,13 @@
+package br.com.eco.pointservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PointserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PointserviceApplication.class, args);
+	}
+
+}
---
+++ api/pointservice/src/test/java/br/com/eco/pointservice/PointserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.eco.pointservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PointserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
